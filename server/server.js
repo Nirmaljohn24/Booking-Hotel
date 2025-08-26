9d4f3f188a8109f0f25acc80168a146e48736681
Nirmaljohn24 <johnsondravid143@gmail.com>
2025-08-27 00:48:50 +0530

    Updated code second time

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import clerkWebhooks from './controllers/clerkWebhooks.js';
+import userRouter from './routes/userRoutes.js';
 
 
 connectDB();
@@ -22,6 +23,8 @@ app.use('/api/clerk' , clerkWebhooks);
 app.get('/',(req,res) => {
     res.send('API is Working...')
 });
+
+app.use('/api/user',userRouter)
 
 const PORT = process.env.PORT || 3000;
 
